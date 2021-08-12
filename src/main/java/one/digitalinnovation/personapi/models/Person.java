88feb4ab9ca9fefdacd5d031c1f34753e57ac81f
rafael-a-m-chaves <rafaelaugustochaves@hotmail.com>
2021-08-12 16:54:47 -0300

    Create Model Class

---
+++ src/main/java/one/digitalinnovation/personapi/models/Person.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.personapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REMOVE})
+    private List<Phone> phones = new ArrayList<>();
+}
